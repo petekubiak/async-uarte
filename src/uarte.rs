7ee67df2ff9a7aeacb94bc5d08a6185197e22a19
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-09-15 23:21:22 +0100

    Begin defining RX buffering logic

--- src/uarte.rs
+++ src/uarte.rs
@@ -1,7 +1,30 @@
+use core::sync::atomic::{AtomicBool, AtomicIsize};
+
 use nrf52832_hal::{
     pac::UARTE0,
     uarte::{Baudrate, Pins},
 };
+
+#[derive(Default)]
+struct Block {
+    buffer: [u8; 5],
+    filled: AtomicBool,
+}
+
+static UARTE_BUFFER: [Block; 3] = [
+    Block {
+        buffer: [0; 5],
+        filled: AtomicBool::new(false),
+    },
+    Block {
+        buffer: [0; 5],
+        filled: AtomicBool::new(false),
+    },
+    Block {
+        buffer: [0; 5],
+        filled: AtomicBool::new(false),
+    },
+];
 
 pub struct Uarte {
     uarte: UARTE0,
@@ -30,5 +53,10 @@ impl Uarte {
             .intenset
             .write(|w| w.rxstarted().set_bit().error().set_bit().endrx().set_bit());
         Self { uarte }
+    }
+
+    fn update_rxd_buffer_location(&self) {
+        static WRITE_OFFSET: AtomicIsize = AtomicIsize::new(0);
+        todo!()
     }
 }
