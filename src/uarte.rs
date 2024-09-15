d51c397e9da0bd2cc7cb8672a2165bb65cdde4ed
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-09-15 23:20:24 +0100

    Add more UARTE intialisation (WIP)

--- src/uarte.rs
+++ src/uarte.rs
@@ -9,9 +9,26 @@ pub struct Uarte {
 
 impl Uarte {
     pub fn new(pins: Pins, uarte: UARTE0, baud_rate: Baudrate) -> Self {
+        // Enable UARTE peripheral
+        uarte.enable.write(|w| {
+            w.enable()
+                .variant(nrf52832_hal::pac::uarte0::enable::ENABLE_A::ENABLED)
+        });
+
+        // Set RX and TX pins
         uarte.psel.rxd.write(|w| w.pin().variant(pins.rxd.pin()));
         uarte.psel.txd.write(|w| w.pin().variant(pins.txd.pin()));
+
+        // Set baud rate
         uarte.baudrate.write(|w| w.baudrate().variant(baud_rate));
+
+        // Initialise the ENDRX -> STARTRX shortcut
+        uarte.shorts.write(|w| w.endrx_startrx().set_bit());
+
+        // Enable interrupt for RXSTARTED, ERROR and ENDRX events
+        uarte
+            .intenset
+            .write(|w| w.rxstarted().set_bit().error().set_bit().endrx().set_bit());
         Self { uarte }
     }
 }
