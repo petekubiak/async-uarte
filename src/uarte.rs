671ea388260003ec6467cdb351226c9bcec8725b
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-09-22 00:43:28 +0100

    [WIP] Refactoring UARTE

--- src/uarte.rs
+++ src/uarte.rs
@@ -1,9 +1,15 @@
-use core::sync::atomic::{AtomicBool, AtomicIsize};
+use core::{
+    cell::Cell,
+    sync::atomic::{AtomicBool, AtomicUsize, Ordering},
+};
 
+use critical_section::Mutex;
 use nrf52832_hal::{
     pac::UARTE0,
     uarte::{Baudrate, Pins},
 };
+
+const BUFFER_SLOTS_COUNT: usize = 3;
 
 #[derive(Default)]
 struct Block {
@@ -11,52 +17,64 @@ struct Block {
     filled: AtomicBool,
 }
 
-static UARTE_BUFFER: [Block; 3] = [
-    Block {
-        buffer: [0; 5],
-        filled: AtomicBool::new(false),
-    },
-    Block {
-        buffer: [0; 5],
-        filled: AtomicBool::new(false),
-    },
-    Block {
-        buffer: [0; 5],
-        filled: AtomicBool::new(false),
-    },
-];
-
-pub struct Uarte {
-    uarte: UARTE0,
+struct Uarte0 {
+    inner: UARTE0,
+    buffer: [Block; BUFFER_SLOTS_COUNT],
 }
 
-impl Uarte {
-    pub fn new(pins: Pins, uarte: UARTE0, baud_rate: Baudrate) -> Self {
+static UARTE0_INSTANCE: Mutex<Cell<Option<Uarte0>>> = Mutex::new(Cell::new(None));
+
+impl Uarte0 {
+    pub fn init(pins: Pins, peripheral: UARTE0, baud_rate: Baudrate) {
+        // Populate the static instance
+        critical_section::with(|cs| {
+            UARTE0_INSTANCE.borrow(cs).set(Some(Self {
+                inner: peripheral,
+                buffer: core::array::from_fn(|_| Block::default()),
+            }));
+        });
+
         // Enable UARTE peripheral
-        uarte.enable.write(|w| {
+        peripheral.enable.write(|w| {
             w.enable()
                 .variant(nrf52832_hal::pac::uarte0::enable::ENABLE_A::ENABLED)
         });
 
         // Set RX and TX pins
-        uarte.psel.rxd.write(|w| w.pin().variant(pins.rxd.pin()));
-        uarte.psel.txd.write(|w| w.pin().variant(pins.txd.pin()));
+        peripheral
+            .psel
+            .rxd
+            .write(|w| w.pin().variant(pins.rxd.pin()));
+        peripheral
+            .psel
+            .txd
+            .write(|w| w.pin().variant(pins.txd.pin()));
 
         // Set baud rate
-        uarte.baudrate.write(|w| w.baudrate().variant(baud_rate));
+        peripheral
+            .baudrate
+            .write(|w| w.baudrate().variant(baud_rate));
 
         // Initialise the ENDRX -> STARTRX shortcut
-        uarte.shorts.write(|w| w.endrx_startrx().set_bit());
+        peripheral.shorts.write(|w| w.endrx_startrx().set_bit());
 
         // Enable interrupt for RXSTARTED, ERROR and ENDRX events
-        uarte
+        peripheral
             .intenset
             .write(|w| w.rxstarted().set_bit().error().set_bit().endrx().set_bit());
-        Self { uarte }
+
+        // Set up UARTE DMA
+        uarte.inner.rxd.maxcnt.write(|w| w.maxcnt().variant(5));
+        uarte.update_rxd_buffer_location();
     }
 
     fn update_rxd_buffer_location(&self) {
-        static WRITE_OFFSET: AtomicIsize = AtomicIsize::new(0);
-        todo!()
+        static WRITE_OFFSET: AtomicUsize = AtomicUsize::new(0);
+        let new_offset = (WRITE_OFFSET.load(Ordering::Acquire) + 1) % BUFFER_SLOTS_COUNT;
+        self.inner.rxd.ptr.write(|w| {
+            w.ptr()
+                .variant(self.buffer[new_offset].buffer.as_ptr() as u32)
+        });
+        WRITE_OFFSET.store(new_offset, Ordering::Release);
     }
 }
