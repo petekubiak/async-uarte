4cd59d0f2209efc12b282eebad8bc25f50a22558
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-09-29 00:57:56 +0100

    Complete uarte implementation

--- src/uarte.rs
+++ src/uarte.rs
@@ -1,9 +1,7 @@
-use core::{
-    cell::Cell,
-    sync::atomic::{AtomicBool, AtomicUsize, Ordering},
-};
+use core::{cell::RefCell, ops::DerefMut as _};
 
 use critical_section::Mutex;
+use nrf52832_hal::pac::interrupt;
 use nrf52832_hal::{
     pac::UARTE0,
     uarte::{Baudrate, Pins},
@@ -14,26 +12,17 @@ const BUFFER_SLOTS_COUNT: usize = 3;
 #[derive(Default)]
 struct Block {
     buffer: [u8; 5],
-    filled: AtomicBool,
+    filled: bool,
 }
 
 struct Uarte0 {
     inner: UARTE0,
     buffer: [Block; BUFFER_SLOTS_COUNT],
+    write_offset: usize,
 }
-
-static UARTE0_INSTANCE: Mutex<Cell<Option<Uarte0>>> = Mutex::new(Cell::new(None));
 
 impl Uarte0 {
     pub fn init(pins: Pins, peripheral: UARTE0, baud_rate: Baudrate) {
-        // Populate the static instance
-        critical_section::with(|cs| {
-            UARTE0_INSTANCE.borrow(cs).set(Some(Self {
-                inner: peripheral,
-                buffer: core::array::from_fn(|_| Block::default()),
-            }));
-        });
-
         // Enable UARTE peripheral
         peripheral.enable.write(|w| {
             w.enable()
@@ -64,17 +53,54 @@ impl Uarte0 {
             .write(|w| w.rxstarted().set_bit().error().set_bit().endrx().set_bit());
 
         // Set up UARTE DMA
-        uarte.inner.rxd.maxcnt.write(|w| w.maxcnt().variant(5));
-        uarte.update_rxd_buffer_location();
+        peripheral.rxd.maxcnt.write(|w| w.maxcnt().variant(5));
+
+        let mut instance = Self {
+            inner: peripheral,
+            buffer: core::array::from_fn(|_| Block::default()),
+            write_offset: 0,
+        };
+        instance.update_rxd_buffer_location();
+
+        //Enable UARTE interrupt in NVIC
+        unsafe { nrf52832_hal::pac::NVIC::unmask(nrf52832_hal::pac::Interrupt::UARTE0_UART0) };
+
+        // Start UARTE and populate the static instance
+        critical_section::with(|cs| {
+            instance.inner.tasks_startrx.write(|w| unsafe { w.bits(1) });
+            UARTE0_INSTANCE.replace(cs, Some(instance));
+        });
     }
 
-    fn update_rxd_buffer_location(&self) {
-        static WRITE_OFFSET: AtomicUsize = AtomicUsize::new(0);
-        let new_offset = (WRITE_OFFSET.load(Ordering::Acquire) + 1) % BUFFER_SLOTS_COUNT;
+    fn update_rxd_buffer_location(&mut self) {
+        self.write_offset = (self.write_offset + 1) % BUFFER_SLOTS_COUNT;
+        assert!(
+            !self.buffer[self.write_offset].filled,
+            "UARTE buffer overflow!"
+        );
         self.inner.rxd.ptr.write(|w| {
             w.ptr()
-                .variant(self.buffer[new_offset].buffer.as_ptr() as u32)
+                .variant(self.buffer[self.write_offset].buffer.as_ptr() as u32)
         });
-        WRITE_OFFSET.store(new_offset, Ordering::Release);
     }
+}
+
+static UARTE0_INSTANCE: Mutex<RefCell<Option<Uarte0>>> = Mutex::new(RefCell::new(None));
+
+#[interrupt]
+fn UARTE0_UART0() {
+    critical_section::with(|cs| {
+        if let Some(instance) = UARTE0_INSTANCE.borrow_ref_mut(cs).deref_mut() {
+            if instance.inner.events_rxstarted.read().bits() == 1 {
+                instance.update_rxd_buffer_location();
+                instance.inner.events_rxstarted.reset();
+            } else if instance.inner.events_endrx.read().bits() == 1 {
+                instance.buffer[instance.write_offset].filled = true;
+                instance.inner.events_endrx.reset();
+            } else if instance.inner.events_error.read().bits() == 1 {
+                instance.inner.tasks_flushrx.write(|w| unsafe { w.bits(1) });
+                instance.inner.events_error.reset();
+            }
+        }
+    })
 }
